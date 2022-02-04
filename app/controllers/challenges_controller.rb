01695de55909a5781325a2ada5d2963c773f4720
Edinson Gutierrez <edinsongutierrez29@gmail.com>
2022-02-04 00:04:34 -0500

    Se crea modelo y controlador de Challenge

---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,14 @@
+class ChallengesController < ApplicationController
+  def index
+    @challenges = Challenge.all
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'challenges/index'
+  get 'challenges/create'
+  get 'challenges/update'
+  get 'challenges/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :challenges
 end
